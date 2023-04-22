5bd539f2222d1ff3452eafc7ce9f0b67f5d82d08
divyatellabati26 <95356480+divyatellabati26@users.noreply.github.com>
2023-04-22 19:38:53 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,71 +1,58 @@
-var newTask = document.querySelector('#new-task');
-var addTaskBtn = document.querySelector('#addTask');
-var toDoUl = document.querySelector(".todo-list ul");
-var completeUl =  document.querySelector(".complete-list ul");
-
-var createNewTask = function(task){
-  console.log("Creating task...");
-  var listItem = document.createElement("li"); 
-  var checkBox = document.createElement("input"); 
-  var label = document.createElement("label"); 
-  label.innerText = task;
-  checkBox.type = "checkbox";
-  listItem.appendChild(checkBox);
-  listItem.appendChild(label);
-  return listItem;  
-  
-};
-
-var addTask = function(){
-  console.log("Adding task...");
-  var listItem = createNewTask(newTask.value);
-  toDoUl.appendChild(listItem); 
-  newTask.value="";
-  bindIncompleteItems(listItem, completeTask);
-
-};
-
-var completeTask = function(){
-  var listItem = this.parentNode;
-  var deleteBtn = document.createElement("button"); 
-  deleteBtn.innerText ="Delete"; 
-  deleteBtn.className = "delete";
-  listItem.appendChild(deleteBtn);
-  var checkBox = listItem.querySelector("input[type=checkbox]");
-  checkBox.remove();
-  completeUl.appendChild(listItem); 
-  bindCompleteItems(listItem, deleteTask);
-  
-};
-
-var deleteTask = function(){
-  console.log("Deleting task...");
-  var listItem = this.parentNode;
-  var ul = listItem.parentNode;
-  ul.removeChild(listItem);
-  
-};
-
-var bindIncompleteItems = function(taskItem, checkBoxClick){  
-  console.log("Binding the incomplete list...");
-  var checkBox = taskItem.querySelector("input[type=checkbox]");
-  checkBox.onchange = checkBoxClick;  
-}; 
-
-var bindCompleteItems = function(taskItem, deleteButtonPress){
-  console.log("Binding the complete list...");
-  var deleteButton = taskItem.querySelector(".delete");
-  deleteButton.onclick = deleteButtonPress;
-    
-};
-
-for(var i=0; i < toDoUl.children.length; i++) {
-  bindIncompleteItems(toDoUl.children[i], completeTask);
+function temperature() {
+    let inputValue = document.querySelector("#inputValue").value;
+    let type1 = document.querySelector("#type1");
+    let type2 = document.querySelector("#type2");
+    let result = document.querySelector("#result");
+    if (inputValue == "") {
+        alert("Please Enter any Number")
+        location.reload()
+    }
+    else if (type1.value == "celcius" && type2.value == "celcius") {
+        let Fahrenheit = Number.parseInt(inputValue) * (9 / 5) + 32
+        result.innerHTML = Fahrenheit.toFixed(3) + "F"
+    }
+    else if (type1.value == "celcius" && type2.value == "fahrenheit") {
+        let Celcius = Number.parseInt(inputValue) * 1
+        result.innerHTML =Celcius.toFixed(3) + ` &deg;C`
+    }
+    else if (type1.value == "celcius" && type2.value == "kelvin") {
+        let Kelvin = Number.parseInt(inputValue) + 273.15
+        result.innerHTML = Kelvin.toFixed(3) + " K"
+    }
+    else if (type1.value == "fahrenheit" && type2.value == "celcius") {
+        let Celcius = Number.parseInt(inputValue) - 32 * (5 / 9)
+        result.innerHTML = Celcius.toFixed(3) + ` &deg;C`
+    }
+    else if (type1.value == "fahrenheit" && type2.value == "fahrenheit") {
+        let Fahrenheit = Number.parseInt(inputValue) * 1
+        result.innerHTML = Fahrenheit.toFixed(3) + " F"
+    }
+    else if (type1.value == "fahrenheit" && type2.value == "kelvin") {
+        let Kelvin = Number.parseInt(inputValue) - 32 * (5 / 9) + 273.15
+        result.innerHTML = Kelvin.toFixed(3) + " K"
+    }
+    else if (type1.value == "kelvin" && type2.value == "celcius") {
+        let Celcius = Number.parseInt(inputValue) - 273.15
+        result.innerHTML = Celcius.toFixed(3) + ` &deg;C`
+    }
+    else if (type1.value == "kelvin" && type2.value == "fahrenheit") {
+        let Fahrenheit = Number.parseInt(inputValue) - 273.15 * (9 / 5) + 32
+        result.innerHTML = Fahrenheit.toFixed(3) + " F"
+    }
+    else if (type1.value == "kelvin" && type2.value == "kelvin") {
+        let Kelvin = Number.parseInt(inputValue) * 1
+        result.innerHTML = Kelvin.toFixed(3) + " K"
+    }
+    else if (type1.value == "rankine" && type2.value == "celcius") {
+        let Celcius = Number.parseInt(inputValue) - 491.67 * (5 / 9)
+        result.innerHTML = Celcius.toFixed(3) + ` &deg;C`
+    }
+    else if (type1.value == "rankine" && type2.value == "fahrenheit") {
+        let Fahrenheit = Number.parseInt(inputValue) - 495.67
+        result.innerHTML = Fahrenheit.toFixed(3) + " F"
+    }
+    else if (type1.value == "rankine" && type2.value == "kelvin") {
+        let Kelvin = Number.parseInt(inputValue) * (5 / 9)
+        result.innerHTML = Kelvin.toFixed(3) + " K"
+    }
 }
-
-for(var i=0; i < completeUl.children.length; i++) {
-  bindCompleteItems(completeUl.children[i], deleteTask);
-}
-
-addTaskBtn.addEventListener("click", addTask);
-
